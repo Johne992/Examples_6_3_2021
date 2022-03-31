50d94292a9a244f5bb0d9e1159f4b574abdaa3f8
John Lewis <johnelewis92@gmail.com>
2022-03-31 16:43:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// like a variable
+document.getElementById('exdiv').style.backgroundColor = 'green';
+// like an array
+document.getElementsByClassName('art-div')[0].style.fontSize='5em';
+document.getElementsByClassName('art-div random')[0].style.fontSize='5em';
